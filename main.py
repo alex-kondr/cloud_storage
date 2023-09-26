495a922413508d6f328d299f9b21a07d5b5b6be2
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-09-26 10:33:22 +0300

    save

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-import pathlib
 import shutil
+from pathlib import Path
 
 import uvicorn
 
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
 
+
 app = FastAPI()
+
+FILES_PATH = Path("files")
+FILES_PATH.mkdir(exist_ok=True)
 
 
 @app.post("/")
 async def upload_file(file: UploadFile):
-    with open(file.filename, "wb") as fh:
+    with open(FILES_PATH / file.filename, "wb") as fh:
         shutil.copyfileobj(file.file, fh)
 
 
 @app.get("/", response_class=FileResponse)
 async def find_file(file_name: str):
-    return file_name
+    return FILES_PATH / file_name
 
 
 if __name__ == '__main__':
